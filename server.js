2940ca6fb6ca7061cb1dd29733f4a81b888d1073
jvmMachado <joaovmmachado@gmail.com>
2020-02-20 18:11:39 -0300

    add heroku postgres connection and set up for deploy

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
 
 const server = express();
 server.use(express.urlencoded({ extended: true }));
 
 server.use(express.static('public'));
 
-const Pool = require('pg').Pool
-const db = new Pool({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'doe'
-});
+// const Pool = require('pg').Pool;
+// const db = new Pool({
+//   user: 'qedjzivuftafds',
+//   password: 'f88d796c766fbc4cb96ecdb50817881f8b43115d123b2794c09f42bfad6b2ac1',
+//   host:
+//     'postgres://qedjzivuftafds:f88d796c766fbc4cb96ecdb50817881f8b43115d123b2794c09f42bfad6b2ac1@ec2-3-234-109-123.compute-1.amazonaws.com:5432/ddk4cv39m71m4q',
+//   port: 5432,
+//   database: 'ddk4cv39m71m4q',
+//   ssl: { rejectUnauthorized: false }
+// });
 
+// db.connect();
 
 nunjucks.configure('./', {
   express: server,
   noCache: true
 });
 
-
-
 server.get('/', (req, res) => {
-  
-  db.query(`SELECT * FROM donors`, (err, donorsFound)=> {
+  client.query(`SELECT * FROM donors`, (err, donorsFound) => {
     if (err) return res.send('Erro no banco de dados.');
 
     const donors = donorsFound.rows;
@@ -35,9 +43,9 @@ server.get('/', (req, res) => {
 });
 
 server.post('/', (req, res) => {
-  const name = req.body.name
-  const email = req.body.email
-  const blood = req.body.blood
+  const name = req.body.name;
+  const email = req.body.email;
+  const blood = req.body.blood;
 
   if (name == '' || email == '' || blood == '') {
     return res.send('Todos os campos são obrigatórios.');
@@ -45,11 +53,11 @@ server.post('/', (req, res) => {
 
   const query = `
   INSERT INTO donors ("name", "email", "blood")
-  VALUES ($1, $2, $3)`
+  VALUES ($1, $2, $3)`;
 
-  const values = [name, email, blood]
+  const values = [name, email, blood];
 
-  db.query(query, values, (err) =>{
+  client.query(query, values, err => {
     if (err) return res.send('Erro no banco de dados.');
 
     return res.redirect('/');
