dfe9d5fa4b34e44df5b54c3dbd98d97bdd0f679a
jvmMachado <joaovmmachado@gmail.com>
2020-02-20 18:17:43 -0300

    change server port

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ let port = process.env.PORT;
 if (port == null || port == '') {
   port = 3000;
 }
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
