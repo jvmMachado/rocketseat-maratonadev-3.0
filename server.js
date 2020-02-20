23953545018a61504b3370ec54f948f7061b7670
jvmMachado <joaovmmachado@gmail.com>
2020-02-20 18:23:08 -0300

    add SQL query

--- server.js
+++ server.js
@@ -7,13 +7,23 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
-
-client.connect();
-
 const server = express();
 server.use(express.urlencoded({ extended: true }));
 
 server.use(express.static('public'));
+
+client.connect();
+
+client.query(
+  'SELECT table_schema,table_name FROM information_schema.tables;',
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 // const Pool = require('pg').Pool;
 // const db = new Pool({
