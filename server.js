4ead5fe96ca869aa17d1c64d0f5caee971d56359
jvmMachado <joaovmmachado@gmail.com>
2020-02-20 18:19:22 -0300

    change ssl configs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: { rejectUnauthorized: false }
 });
 
 client.connect();
